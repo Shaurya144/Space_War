0fdcb02d542582ea2966661e794dfd956016b5fd
Shaurya144 <116948636+Shaurya144@users.noreply.github.com>
2022-10-29 10:56:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,21 @@ WIDTH, HEIGHT = 750, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invasion")
 
-# Load images
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 ALIEN = pygame.image.load(os.path.join("assets", "space_alien .png"))
 ALIEN = pygame.transform.scale(ALIEN, (50, 50))
-# Player player
+
 YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 SPACE_SHIP = pygame.image.load(os.path.join("assets", "space_ship.png"))
 SPACE_SHIP = pygame.transform.scale(SPACE_SHIP, (100, 100))
 
-# Lasers
 RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
 GREEN_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_green.png"))
 BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 
-
-# Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 class Laser:
